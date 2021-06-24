e4cc804b85caca324a997c3596343ed911c8b37b
ABIY <ABIY@10.0.0.236>
2021-06-24 17:51:49 -0400

    The basic calculator is commited

---
+++ Abiy Gitpro/src/javaCodes/GitDemo.java
@@ -0,0 +1,34 @@
+package javaCodes;
+
+public class GitDemo {
+
+	public static void calculateForme(int num1 , int num2, String calculate) {
+		int firstNumber = num1;
+		int secondNumber = num2;
+		//int total=0;
+		//String calculate = "calculate";
+		switch(calculate) {
+		case  "+" :
+		System.out.println(firstNumber+secondNumber);
+	    break;
+		case "-" :
+			System.out.println(firstNumber-secondNumber);
+			break;
+		case "*" :
+			System.out.println(firstNumber*secondNumber);
+			break;
+		case "/" :
+			System.out.println(firstNumber/secondNumber);
+			
+			
+		}
+	
+	}
+	
+	
+	public static void main(String[] args) {
+	calculateForme(40,30 ,"+");	
+
+	}
+
+}
